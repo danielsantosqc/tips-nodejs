280a0c68b62416874dc6fabf00a172c38f5eb3bd
Daniel Quispe <danielsantosqc.dev@gmail.com>
2022-01-22 11:15:47 -0600

    comenzamos

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const frutas = require('./frutas');
 // console.log('Hola desde app.js');
 
 frutas.forEach(item => console.log(item));
+console.log('Hice unos cambios'); 
+console.log('Hice unos cambios1'); 
+console.log('Hice unos cambios2'); 
+
+
 
 
 
